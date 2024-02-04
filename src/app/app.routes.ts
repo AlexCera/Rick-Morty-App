fcfd886dfc739ad416ea66c1c5e04e971d3eea7a
Alex Cera <devalexcera@gmail.com>
2024-02-04 14:42:18 -0500

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'splash',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'character',
+    loadComponent: () => import('./pages/character/character.page').then( m => m.CharacterPage)
+  },
+  {
+    path: 'splash',
+    loadComponent: () => import('./pages/splash/splash.page').then( m => m.SplashPage)
+  },
+];
